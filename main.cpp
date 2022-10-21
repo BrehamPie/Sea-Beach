da93bf552e68b4048762ff6a41c456375426b017
BrehamPie <brehampie@gmail.com>
2022-10-21 07:41:25 +0600

    Init

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include<GL/gl.h>
+#include<GL/glu.h>
+#include<GL/glut.h>
+#include<windows.h>
+#include<bits/stdc++.h>
+
+//Global Variables
+GLfloat eye[3] = {},look[3]={};
+GLfloat up[3]= {0,1,0};
+float fovy=90;
+
+//external header files
+
+int main(){}
